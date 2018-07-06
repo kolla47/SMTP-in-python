c2eb46259a6174e0196961bfda503a458152ffad
kolla47 <akhil.trinayan@gmail.com>
2018-07-06 17:14:09 +0530

    SMTP

---
+++ mailsender.py
@@ -0,0 +1,17 @@
+import smtplib
+gmail_user = "akhil.triayan@gmail.com"
+gmail_pwd = "xxxxxxxx"
+TO = 'akhil.trinayan@gmail.com'
+SUBJECT = "Testing sending using gmail"
+TEXT = "Testing sending mail using gmail servers"
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmail_user, gmail_pwd)
+BODY = '\r\n'.join(['To: %s' % TO,
+        'From: %s' % gmail_user,
+        'Subject: %s' % SUBJECT,
+        '', TEXT])
+
+server.sendmail(gmail_user, [TO], BODY)
+print ('email sent')
